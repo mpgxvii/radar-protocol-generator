8db755101b6479dd0ddffe4c3eff520ee2f39939
Pauline <mpgxvii@gmail.com>
2024-03-18 14:52:33 +0000

    Add deployment files

---
+++ src/environments/environment.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: true,
+  // base url of the rest-sources authorizer app
+  baseHref: 'BASE_HREF',
+  radarBaseUrl: 'RADAR_BASE_URL',
+  // base url of the rest-sources-auth-backend
+  backendBaseUrl: 'BACKEND_BASE_URL',
+  // If user validation is enabled
+  doValidate: 'VALIDATE',
+  // Grant-type of authorization
+  authorizationGrantType: 'AUTH_GRANT_TYPE',
+  // Client id of the authorizer app.
+  appClientId: 'AUTH_CLIENT_ID',
+  // Client secret of the authorizer app.
+  appClientSecret: 'AUTH_CLIENT_SECRET',
+  // Callback URL registered in MP.
+  authCallbackUrl: 'AUTH_CALLBACK_URL',
+  // Management Portal URL.
+  authBaseUrl: 'AUTH_URI'
+};
